4bbba2dae0249fcffa348b0dcf47453a7676cca3
helionext <juniorjobs.zen@gmail.com>
2023-09-08 19:35:17 -0300

    crud simples

---
+++ Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoDio.Models;
+
+namespace ToDoDio.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoDio.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
